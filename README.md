95c777ed595dea493c7a297190d6fcced0f878af
SriRam_N <89205957+SriramN7@users.noreply.github.com>
2025-10-22 08:04:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+VaalamTechnologies_Website
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'neon-orange': '#FF5F1F',
+      },
+    },
+  },
+  plugins: [],
+};
